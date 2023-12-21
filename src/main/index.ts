71504fd43e82b1205875694d8a154ab5648d0ea5
DewitteRuben <rubendewitte1998@gmail.com>
2023-12-21 09:39:03 +0100

    chore: add title

--- src/main/index.ts
+++ src/main/index.ts
@@ -14,6 +14,7 @@ function createWindow() {
     frame: true,
     resizable: false,
     maximizable: false,
+    title: 'Reflasher',
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
